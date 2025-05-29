361f549401b1d01f3b24d971787d460b101d1435
PedroX157 <163874193+PedroX157@users.noreply.github.com>
2025-05-29 10:47:27 -0300

    29/05/2025

---
+++ scripr.js
@@ -0,0 +1,9 @@
+ document.addEventListener('DOMContentLoaded',function(){
+    const botaoDeAcessibilidade = document.getElementById ('botao=acessibilidade')
+     const opcoesDeAcessibilidade = document.getElementById ('botao=acessibilidade')
+ }
+     botaoDeAcessibilidade.addEventListener('click', fuction)(){
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao')
+     botaoDeAcessibilidade.classList.toggle('apresenta-lista')
+     })
+    
